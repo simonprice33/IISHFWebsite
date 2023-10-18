cbea413be6f7dd91b9e0cb03b0bcc527f7511da4
Simon Price <Simon.Price@sjp-it.co.uk>
2023-10-18 21:45:16 +0100

    added new pages

---
+++ IISHFTest/Helpers/FileIconHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace IISHFTest.Helpers
+{
+    public class FileIconHelper
+    {
+        private static readonly Dictionary<string, string> ExtensionIconMap = new Dictionary<string, string>
+        {
+            { "pdf", "fa-light fa-file-pdf " },
+            { "doc", "fa-light fa-file-word " },
+            { "docx", "fa-light fa-file-word " },
+            { "xls", "ffa-sharp fa-light fa-file-excel " },
+            { "xlsx", "ffa-sharp fa-light fa-file-excel " }
+        };
+
+        public static string GetIcon(string fileUrl)
+        {
+            string fileExtension = fileUrl.Split('.').Last().ToLower();
+            return ExtensionIconMap.TryGetValue(fileExtension, out string icon) ? icon : string.Empty;
+        }
+    }
+}
